a54d6d060147ab448dd8d2b88b8b9ba301d64729
Nelson Maldonado <play2nino@gmail.com>
2021-08-11 19:00:23 -0700

    first saved

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from "react";
-import "./App.css";
+import React from "react"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
       <p>
+        <h1>This app is up and running</h1>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun{" "}
+        <span role="img" aria-label="go!">
+          🚀
+        </span>
+        !
       </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
